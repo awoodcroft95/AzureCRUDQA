a89b2a94477f2526c830d8da520edc38d6219829
awoodcroft95 <awoodcroft95@gmail.com>
2019-01-22 21:11:26 +0000

    Add project files.

---
+++ AzureCRUDQA/Data/AzureCRUDQAContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCRUDQA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureCRUDQA.Data
+{
+    public class AzureCRUDQAContext : DbContext
+    {
+        public AzureCRUDQAContext(DbContextOptions<AzureCRUDQAContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Accounts> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Accounts>().ToTable("Accounts");
+        }
+    }
+}
---
+++ AzureCRUDQA/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCRUDQA.Models;
+
+namespace AzureCRUDQA.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AzureCRUDQAContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Accounts.Any())
+            {
+                return;
+            }
+
+            var accounts = new Accounts[]
+            {
+                new Accounts{AccountName="Test1", AccountPassword="Testpass1", AccountDeatils="test details", CreateDate=DateTime.Now, UpdateDate=DateTime.Now},
+                new Accounts{AccountName="Test2", AccountPassword="Testpass2", AccountDeatils="test details", CreateDate=DateTime.Now, UpdateDate=DateTime.Now},
+                new Accounts{AccountName="Test3", AccountPassword="Testpass3", AccountDeatils="test details", CreateDate=DateTime.Now, UpdateDate=DateTime.Now}
+            };
+
+            foreach(var a in accounts)
+            {
+                context.Accounts.Add(a);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
